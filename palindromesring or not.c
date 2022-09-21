161a99ddd0be50ee3f51a741c9eb9fd1763802a2
darbar-123 <112846580+darbar-123@users.noreply.github.com>
2022-09-21 09:11:50 +0530

    Add files via upload

---
+++ palindromesring or not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str1[50],str2[50];
+printf("enter the string");
+gets(str1);
+strcpy(str2,str1);
+strrev(str2);
+if(strcmp(str1,str2)==0)
+printf("string is palindrome:");
+else
+printf("string is not palindrome:");
+return 0;
+}
