e7ebb7f5d4c9dc586bf45b9b476ae611d6687eaa
darbar-123 <112846580+darbar-123@users.noreply.github.com>
2022-09-21 09:31:48 +0530

    Update largest number of an array.c

--- largest number of an array.c
+++ largest number of an array.c
@@ -9,15 +9,11 @@ int main() {
     printf("Enter number%d: ", i + 1);
     scanf("%lf", &arr[i]);
   }
-
-  // storing the largest number to arr[0]
   for (int i = 1; i < n; ++i) {
     if (arr[0] < arr[i]) {
       arr[0] = arr[i];
     }
   }
-
   printf("Largest element = %.2lf", arr[0]);
-
   return 0;
 }
